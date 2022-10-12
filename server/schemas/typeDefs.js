1cb5bf09493359cdc64c6bc41ad95ea350623af3
rockwoodc <rockwoodce@gmail.com>
2022-10-11 20:29:57 -0400

    defined more queries and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,17 @@
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
-
 // create our typeDefs
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
 type Thought {
     _id: ID
     thoughtText: String
@@ -19,10 +27,14 @@ type Thought {
     createdAt: String
     username: String
   }
-  
+
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
-  }`
+    thought(_id: ID!): Thought
+  }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
